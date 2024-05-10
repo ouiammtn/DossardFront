96227b4093976975039371a5dedecae7d23f4ae9
mmedj <mohamedjaouhar1999@gmail.com>
2024-05-10 23:20:06 +0100

    Update upload.js

--- src/components/upload.js
+++ src/components/upload.js
@@ -1,105 +1,97 @@
-import "../styles/upload.css";
 import React, { useState } from "react";
+import Axios from "axios";
 import upload from "../assets/upload.svg";
-import axios from "axios"; // Import Axios for making HTTP requests
 
 const Upload = () => {
   const [files, setFiles] = useState([]);
   const [uploadProgress, setUploadProgress] = useState(0);
 
-  const handleFileChange = (e) => {
-    const fileList = Array.from(e.target.files);
+  const handleFileChange = (event) => {
+    const fileList = Array.from(event.target.files);
     setFiles(fileList);
-
-    // Preview images
-    const previews = [];
-    fileList.forEach((file) => {
-      const reader = new FileReader();
-      reader.onload = (e) => {
-        previews.push({ file, src: e.target.result });
-        setFiles([...previews]);
-      };
-      reader.readAsDataURL(file);
-    });
   };
 
-  const handleSubmit = async () => {
-    // Create a new FormData object
-    const formData = new FormData();
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-    // Append each file to the FormData object
-    files.forEach((file) => {
-      formData.append("files", file);
-    });
-    console.log(formData);
     try {
-      // Make a POST request to the API endpoint with the FormData object
-      const response = await axios.post("/upload", formData, {
+      const formData = new FormData();
+      files.forEach((file) => {
+        formData.append("files", file);
+      });
+
+      const config = {
         headers: {
-          "Content-Type": "multipart/form-data", // Set content type to multipart/form-data
+          "Content-Type": "multipart/form-data",
         },
         onUploadProgress: (progressEvent) => {
-          // Update upload progress
           const progress = Math.round(
             (progressEvent.loaded / progressEvent.total) * 100
           );
           setUploadProgress(progress);
         },
-      });
+      };
 
-      // Handle successful response
-      console.log("Upload successful", response);
-      // Optionally, you can do something with the response data here
+      await Axios.post(
+        "http://127.0.0.1:5000/photograph/upload?files",
+        formData,
+        config
+      );
+
+      // Reset files after successful upload
+      setFiles([]);
+      setUploadProgress(0);
+
+      console.log("Files uploaded successfully");
     } catch (error) {
-      // Handle error
-      console.error("Error uploading files", error);
+      console.error("Error uploading files:", error);
     }
   };
 
   return (
-    <>
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-12">
-            <div className="form-group az">
-              <h2>Upload Your Pictures Here : </h2>
-              <div className="image-upload">
-                <input
-                  type="file"
-                  className="form-control"
-                  onChange={handleFileChange}
-                  multiple
-                />
-                <div className="image-uploads">
-                  <img src={upload} alt="upload icon" />
-                  <h4>Drag and drop a file to upload</h4>
-                  <div className="uploads">
-                    {files.map((item, index) => (
-                      <div key={index} className="image-item">
-                        <img src={item.src} alt={`upload ${index}`} />
-                        <p>
-                          {item.file
-                            ? item.file.name.length > 10
-                              ? `${item.file.name.substring(
-                                  0,
-                                  10
-                                )}...${item.file.name.substring(
-                                  item.file.name.lastIndexOf(".") + 1
-                                )}`
-                              : item.file.name
-                            : "File not uploaded"}
-                        </p>
-                      </div>
-                    ))}
+    <div className="container">
+      <form onSubmit={handleSubmit}>
+        <div className="form-group az">
+          <h2>Upload Your Pictures Here:</h2>
+          <div className="image-upload">
+            <input
+              type="file"
+              className="form-control"
+              onChange={handleFileChange}
+              multiple
+            />
+            <div className="image-uploads">
+              <img src={upload} alt="upload icon" />
+              <h4>Drag and drop a file to upload</h4>
+              <div className="uploads">
+                {files.map((file, index) => (
+                  <div key={index} className="image-item">
+                    <img
+                      src={URL.createObjectURL(file)}
+                      alt={`upload ${index}`}
+                    />
+                    <p>
+                      {" "}
+                      {file.name
+                        ? file.name.length > 10
+                          ? `${file.name.substring(
+                              0,
+                              10
+                            )}...${file.name.substring(
+                              file.name.lastIndexOf(".") + 1
+                            )}`
+                          : file.name
+                        : "File not uploaded"}
+                    </p>
                   </div>
-                  <button onClick={handleSubmit}>Upload</button>
-                </div>
+                ))}
               </div>
+              <button type="submit">Upload</button>
             </div>
           </div>
         </div>
-      </div>
-    </>
+      </form>
+    </div>
   );
 };
 
